c395e216c45b56479a117dd1c73f899a23e30917
Hubert Mazur <huberto2000.dom@gmail.com>
2023-04-05 15:56:17 +0200

    Move scene initialization to Scene class

--- app/src/main/java/grak/Main.java
+++ app/src/main/java/grak/Main.java
@@ -1,38 +1,24 @@
 package grak;
 
-import java.util.List;
-
 import grak.geometry.Direction;
 import grak.geometry.Rotation;
-import grak.geometry.drawable.Cuboid;
-import grak.geometry.drawable.Drawable;
-import grak.geometry.drawable.Line;
 
 public class Main {
     
     private static final long KEY_PRESS_DELAY = 50;
 
     public static void main(String[] args) throws InterruptedException {
-        initializeScene();
+        Scene.getInstance().redraw();
+        mainLoop();
+    }
+
+    private static void mainLoop() throws InterruptedException {
         while (true) {
             if (reactToControls()) {
                 Scene.getInstance().redraw();
                 Thread.sleep(KEY_PRESS_DELAY);
             }
         }
-    }
-
-    private static void initializeScene() {
-        List<Drawable> drawables = List.of(
-            new Line(0.9, 0, 0, 0.9, 0, 10),
-            new Line(-0.9, 0, 0, -0.9, 0, 10),
-            new Cuboid(1, 0, 1, 2, -1, 2),
-            new Cuboid(-1, 0, 1, -2, -3, 2),
-            new Cuboid(1, 0, 2.1, 2, -2, 4.1),
-            new Cuboid(-1, 0, 2.1, -1.5, -1.5, 5)
-        );
-        drawables.forEach(Scene.getInstance()::addShape);
-        Scene.getInstance().redraw();
     }
 
     private static boolean reactToControls() {
--- app/src/main/java/grak/Scene.java
+++ app/src/main/java/grak/Scene.java
@@ -1,6 +1,5 @@
 package grak;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.ejml.simple.SimpleMatrix;
@@ -8,15 +7,25 @@ import org.ejml.simple.SimpleMatrix;
 import grak.canvas.Canvas;
 import grak.geometry.Direction;
 import grak.geometry.Rotation;
+import grak.geometry.drawable.Cuboid;
 import grak.geometry.drawable.Drawable;
+import grak.geometry.drawable.Line;
 
 public class Scene {
 
-    private static Scene scene = new Scene();
+    private static final Scene scene = new Scene();
 
-    private List<Drawable> shapes = new ArrayList<>();
+    private final List<Drawable> shapes;
 
     private Scene() {
+        shapes = List.of(
+            new Line(0.9, 0, 0, 0.9, 0, 10),
+            new Line(-0.9, 0, 0, -0.9, 0, 10),
+            new Cuboid(1, 0, 1, 2, -1, 2),
+            new Cuboid(-1, 0, 1, -2, -3, 2),
+            new Cuboid(1, 0, 2.1, 2, -2, 4.1),
+            new Cuboid(-1, 0, 2.1, -1.5, -1.5, 5)
+        );
     }
 
     public static Scene getInstance() {
