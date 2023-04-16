c537156011599d284b11560e68602941c92bf779
Hubert Mazur <huberto2000.dom@gmail.com>
2023-04-16 23:47:49 +0200

    Add demo support

--- app/src/main/java/pl/edu/pw/ee/App.java
+++ app/src/main/java/pl/edu/pw/ee/App.java
@@ -1,5 +1,7 @@
 package pl.edu.pw.ee;
 
+import java.util.Scanner;
+
 import pl.edu.pw.ee.controls.CameraControl;
 
 public class App {
@@ -7,13 +9,23 @@ public class App {
     private static final long FRAME_DELAY = 50;
 
     public static void main(String[] args) throws InterruptedException {
-        Scene.getInstance().redraw();
+        loadDemo();
         while (true) {
             if (CameraControl.getInstance().reactToControls()) {
                 Scene.getInstance().redraw();
                 Thread.sleep(FRAME_DELAY);
             }
         }
+    }
+
+    private static void loadDemo() {
+        try (Scanner input = new Scanner(System.in)) {
+            switch (input.nextInt()) {
+                case 1 -> Scene.getInstance().loadTrianglesDemo();
+                default -> Scene.getInstance().loadCubeDemo();
+            }
+        }
+        Scene.getInstance().redraw();
     }
 
 }
--- app/src/main/java/pl/edu/pw/ee/Scene.java
+++ app/src/main/java/pl/edu/pw/ee/Scene.java
@@ -7,7 +7,9 @@ import java.util.List;
 import org.ejml.simple.SimpleMatrix;
 
 import pl.edu.pw.ee.display.Canvas;
+import pl.edu.pw.ee.display.Color;
 import pl.edu.pw.ee.drawable.Drawable;
+import pl.edu.pw.ee.drawable.Triangle;
 import pl.edu.pw.ee.math.Point;
 import pl.edu.pw.ee.math.versors.Direction;
 import pl.edu.pw.ee.math.versors.Rotation;
@@ -20,7 +22,6 @@ public class Scene {
     private final List<Drawable> shapes = new ArrayList<>();
 
     private Scene() {
-        shapes.addAll(new Cuboid(new Point(1., 2., 3.), new Point(-1., 0., 1.)).toDrawables());
     }
 
     public static Scene getInstance() {
@@ -43,12 +44,23 @@ public class Scene {
         }
     }
 
-    public void addShape(Drawable shape) {
-        synchronized(Scene.class) {
-            if (!shapes.contains(shape)) {
-                shapes.add(shape);
-            }
-        }
+    public void loadCubeDemo() {
+        shapes.addAll(new Cuboid(new Point(10., 20., 30.), new Point(-10., 0., 10.)).toDrawables());
+    }
+
+    public void loadTrianglesDemo() {
+        Point p1 = new Point(-20., 0., 30.);
+        Point p2 = new Point(-30., -30., 50.);
+        Point p3 = new Point(0., -40., 60.);
+        Point p4 = new Point(-20., -10., 30.);
+        Point p5 = new Point(-30., -40., 50.);
+        Point p6 = new Point(0., -50., 60.);
+        Point p7 = new Point(-20., -20., 30.);
+        Point p8 = new Point(-30., -50., 50.);
+        Point p9 = new Point(0., -60., 60.);
+        shapes.add(new Triangle(p1, p2, p3, Color.CYAN));
+        shapes.add(new Triangle(p4, p5, p6, Color.MAGENTA));
+        shapes.add(new Triangle(p7, p8, p9, Color.YELLOW));
     }
 
     public void zoomIn() {
